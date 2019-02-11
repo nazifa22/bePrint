824e4693b718c0e650e8f27b9fbf3bf5c47f347a
Nazifa Rashid <nazifarashid1822@gmail.com>
2019-02-11 17:41:10 +0600

    "complete"

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,24 +1,16 @@
 $(document).ready(function(){
-    // $('div.container-fluid').removeClass('container-fluid').addClass('container');
-    // $('body').css({'background-color': '#000'});
-    // $('.wrapper-background').hide();
-    
-
     $('.main-div').hide();
     
     $('.icon').css({'left': '0'});
     
     $('.icon').click(function(){
-        // $(this).toggle();
         $('.main-div').slideDown(1000);
-        // $(this).css({'left': '308px', 'transition': 'all 0.3s linear'});
         $(this).slideUp(1000);
     });
 
     $('#cancel').click(function(){
         $('.main-div').slideUp(1000);
         $('.icon').slideDown(1000);
-        // $('.icon').css({'left': '0', 'transition': 'all 0.3s linear'});
     });
 
     $('#color-picker').on('input', function(event) {
@@ -45,8 +37,47 @@ $(document).ready(function(){
 
     $('#boxed-button').click(function(){
         $('div.container-fluid').removeClass('container-fluid').addClass('container');
+        
         $('body').addClass('bodyColor').removeClass('bodyBG');
+
+        $('#works > .overlay').hide();
+
+        $('#works .bg').removeClass('bg').addClass('boxed-bg');
+
+        $('#works').find('.title').css({
+            'color': '#fff'
+        });
+
+        $('#works').find('p').css({
+            'color': '#fff'
+        });
+
+        $('#about').find('.title').css({
+            'color': '#fff'
+        });
+
+        $('#about').find('.sub-title').css({
+            'color': '#fff'
+        });
+
+        $('#about').find('p').css({
+            'color': '#fff'
+        });
+
+        $('#about .about-bg').removeClass('about-bg').addClass('boxed-bg');
+
+        $('#contact > .bgColor').removeClass('bgColor').addClass('boxed-bg');
+
+        $('#contact  a').css({
+            'color': '#fff'
+        });
+
+        $('#contact  p').css({
+            'color': '#fff'
+        });
+
         $('.main-div').slideUp(1000);
+        
         $('.icon').slideDown(1000);
     });
 
@@ -54,6 +85,47 @@ $(document).ready(function(){
         $('div.container').removeClass('container').addClass('container-fluid');
         $('.main-div').slideUp(1000);
         $('.icon').slideDown(1000);
+
+        $('#works .boxed-bg').removeClass('boxed-bg').addClass('bg');
+
+        $('#about .boxed-bg').removeClass('boxed-bg').addClass('about-bg');
+
+        $('#works').find('.title').css({
+            'color': '#000'
+        });
+
+        $('#works').find('.sub-title').css({
+            'color': '#000'
+        });
+
+        $('#about').find('.title').css({
+            'color': '#fff'
+        });
+
+        $('#about').find('.sub-title').css({
+            'color': '#fff'
+        });
+
+        $('#about').find('p').css({
+            'color': '#fff'
+        });
+
+        $('#works').find('p').css({
+            'color': '#000'
+        });
+
+        $('#contact > .boxed-bg').removeClass('boxed-bg').addClass('bgColor');
+
+        $('#contact a').css({
+            'color': '#000'
+        });
+
+        $('#contact p').css({
+            'color': '#000'
+        });
+
+        $('#works > .overlay').show();
+        $('#about > .overlay').show();
     });
 
     $('#bg-button').click(function(){
@@ -61,6 +133,32 @@ $(document).ready(function(){
         $('body').addClass('bodyBG').removeClass('bodyColor');
         $('.main-div').slideUp(1000);
         $('.icon').slideDown(1000);
+
+        $('#works > .overlay').hide();
+
+        $('#about  .about-bg').removeClass('about-bg').addClass('boxed-bg');
+
+        $('#about > .overlay').hide();
+
+        $('#works .bg').removeClass('bg');
+
+        $('#works').find('.title').css({
+            'color': '#fff'
+        });
+
+        $('#works').find('.sub-title').css({
+            'color': '#fff'
+        });
+
+        $('#works').find('p').css({
+            'color': '#fff'
+        });
+
+        $('#contact > .bgColor').removeClass('bgColor').addClass('boxed-bg');
+
+        $('#contact  a').css({
+            'color': '#fff'
+        });
     });
     
 });
